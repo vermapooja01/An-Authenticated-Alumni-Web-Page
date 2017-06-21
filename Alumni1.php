0c85668c6c33000d6cf41f65a9b8be68f64abb81
vermapooja01 <vermap90@gmail.com>
2017-06-20 18:05:32 -0600

    The Home Page

---
+++ Alumni1.php
@@ -0,0 +1,37 @@
+<?php
+include 'Userlogin.php';
+
+    echo <<<_END
+<html>
+<head>
+<title>login page</title>
+</head>
+<body>
+<pre>
+<h5 align ='left'>The University of Texas<br/>at El Paso</h5>
+<h1 align ='center'>WELCOME TO UTEP CS ALUMNI ASSOCIATION</h1>
+<p>Join CS Alumni Association of UTEP to connect with your fellow classmates of all ages and stay updated regarding the current events going on in the UTEP campus. <br/>It is a great place to share your memories and thoughts. We also welcome your suggestions regarding the betterment of University and work upon them. </p> 
+_END;
+
+     echo <<<_END
+<p><b>Already a Member?</b></p>
+<form action="Userlogin.php" method = "post">
+username:<input type ="text" name = "username"><br>
+password:<input type ="password" name = "password"><br>
+<input type = "submit" value = "log in" name="submit">
+<p align ='left'>New Users? Connecting is simple. <br/>Register yourself with our Alumni Association.</p>
+<a href = "RegistrationForm.php">REGISTER</a>
+</form>
+</pre>
+</body>
+</html>
+_END;
+
+
+?>
+
+
+
+
+
+
